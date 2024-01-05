65cf53387dedc4401b753619bc4f2ba4fedb87b9
Lennox Charles <lennyaiko17@gmail.com>
2024-01-05 21:25:37 +0100

    progress

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "The ihwiki blog";
-export const SITE_DESCRIPTION = "indie hackers knowledge base";
+export const SITE_DESCRIPTION =
+  "Unlock a wealth of Resources, Insights, Creative Ideas, and Inspirational Nuggets curated by ihwiki, meticulously designed to elevate your journey as an Indie Hacker.";
 export const SITE_HOME = "https://ihwiki.tech";
